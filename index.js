2418fc64fd380db21e5e8429b33a25b22f23eef5
Natalie Chow <cychownat@gmail.com>
2016-01-08 12:14:36 -0800

    Transform color and write to new bmp

--- index.js
+++ index.js
@@ -1,38 +1,64 @@
 const fs = require('fs');
-fs.readFile(__dirname + '/img/non-palette-bitmap.bmp', function(err, data) {
+const transform = require(__dirname + '/lib/transform');
+
+fs.readFile(__dirname + '/img/mario.bmp', function(err, data) {
   if (err) return console.log(err);
-  detectPalette(data);
+  handleTransform(data);
 });
 
 var bitmap = {};
 
+var handleTransform = function(data) {
+  detectPalette(data);
+  convertFromBuf();
+  colorTransform();
+  convertToBuf();
+  writeNewFile();
+};
+
 var detectPalette = function(data) {
+  bitmap.data = data;
   var numColor = data.readUInt32LE(46);
   var pixelStart = data.readUInt32LE(10);
-  bitmap.colorDepth = data.readUInt16LE(28);
-  console.log(bitmap.colorDepth);
   if (numColor) {
     // palette
     console.log('palette');
     bitmap.buf = data.slice(pixelStart - numColor * 4, pixelStart);
-    convertBuf();
+    bitmap.colorDepth = 32;
   } else {
     // non-pa
     console.log('non-palette');
     bitmap.buf = data.slice(pixelStart);
-    convertBuf();
+    bitmap.colorDepth = data.readUInt16LE(28);
   }
 };
 
-var convertBuf = function() {
-  bitmap.rgba = [];
+var colorTransform = function(type) {
+  bitmap.transformed = transform.inverse(bitmap.colors);
+};
+var convertFromBuf = function() {
+  console.log(bitmap.buf);
+  bitmap.colors = [];
   var temp = [];
   for (var i = 0; i < bitmap.buf.length; i++) {
     temp.push(bitmap.buf.readUInt8(i));
     if (temp.length === bitmap.colorDepth / 8) {
-      bitmap.rgba.push(temp);
+      bitmap.colors.push(temp);
       temp = [];
     }
   }
-  console.log(bitmap.rgba.length);
+  console.log(bitmap.colors.length);
+};
+
+var convertToBuf = function() {
+  for (var i = 0; i < bitmap.transformed.length; i++) {
+    for (var j = 0; j < bitmap.transformed[i].length; j++) {
+      bitmap.buf.writeUInt8(bitmap.transformed[i][j], bitmap.transformed[i].length * i + j);
+    }
+  }
+  console.log(bitmap.buf);
+};
+
+var writeNewFile = function() {
+  fs.writeFile(__dirname + '/img/transformed.bmp', bitmap.data);
 };
