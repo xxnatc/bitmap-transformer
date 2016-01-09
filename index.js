d99913316e7600a4efa8fcae5fc0b0445eddf892
Natalie Chow <cychownat@gmail.com>
2016-01-08 22:12:43 -0800

    Refactored index.js, isolated CL handler into its own module, added LE/BE compatibility

--- index.js
+++ index.js
@@ -1,55 +1,30 @@
 const transform = require(__dirname + '/lib/transform');
 const fileHandler = require(__dirname + '/lib/fileHandler');
 const dataHandler = require(__dirname + '/lib/dataHandler');
+const cmdHandler = require(__dirname + '/lib/cmdHandler');
 
 var bitmap = {};
 var index = {};
 
-index.detectPalette = function(data) {
-  bitmap.data = data;
-  var numColor = data.readUInt32LE(46);
-  var pixelStart = data.readUInt32LE(10);
-  if (numColor) {  // palette
-    console.log(numColor + '-color palette');
-    bitmap.buf = data.slice(pixelStart - numColor * 4, pixelStart);
-    bitmap.colorDepth = 32;
-  } else {  // non-palette
-    bitmap.buf = data.slice(pixelStart);
-    bitmap.colorDepth = data.readUInt16LE(28);
-    console.log(bitmap.colorDepth + '-bit non-palette');
-  }
-  console.log('image size: ' + data.readUInt32LE(18) + ' x ' + data.readUInt32LE(22));
-};
-
 index.handleTransform = function(data, type, param) {
-  index.detectPalette(data);
+  dataHandler.process(data, bitmap);
   bitmap.colors = dataHandler.convertFromBuf(bitmap.buf, bitmap.colorDepth / 8);
-  // console.log(bitmap.buf);
   bitmap.transformed = transform[type](bitmap.colors, param);
   dataHandler.updateBuf(bitmap.buf, bitmap.colorDepth / 8, bitmap.transformed);
-  // console.log(bitmap.buf);
   fileHandler.exportNew(bitmap.data);
 };
 
-index.handleCli = function(argv) {
-  if (argv[2] === 'help')
-    return console.log('Color transform methods available: ' + Object.keys(transform).sort().join(', '));
-
-  var transformType = argv[3] || 'inverse';
-  if (transformType.includes('=')) {
-    var param = transformType.split('=')[1];
-    transformType = transformType.split('=')[0];
+index.init = function(list) {
+  if (list) {
+    fileHandler.read(list[0], function(data) {
+      index.handleTransform(data, list[1], list[2]);
+    });
   }
-  if (!transform.hasOwnProperty(transformType.toLowerCase()))
-    return console.log('Color transform method does not exist. Use "help" to see a list of methods.');
-
-  fileHandler.read(argv[2].toLowerCase(), function(data) {
-    index.handleTransform(data, transformType.toLowerCase(), param);
-  });
 };
 
 if (process.argv.length > 2) {
-  index.handleCli(process.argv);
+  var argList = cmdHandler(process.argv);
+  index.init(argList);
 } else {
   console.log('Please specify an image path');
 }
