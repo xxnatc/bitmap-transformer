4ddddbe9e6260a4651a2ba8adf41aad5d2a420be
Yueyue07 <xww.qyy@gmail.com>
2016-01-10 17:07:24 -0800

    add test for cmdHandler.js and fileHandler.js

--- test/test.js
+++ test/test.js
@@ -1,16 +1,48 @@
 var index = require(__dirname + '/../index.js');
-
+var dataHandler = require(__dirname + '/../lib/dataHandler.js');
+var cmdHandler = require(__dirname + '/../lib/cmdHandler.js');
+var fileHandler = require(__dirname + '/../lib/fileHandler.js');
 var expect = require('chai').expect;
 
-describe('Here is bitmap read testing', function(){
-  it('should read as buffer', function(){
-     expect(4+5).to.equal(9);
+describe('test the cmdHandler.js read from index.js file', function() {
+  beforeEach(function() {
+    this.processBackup = process.argv;
+    process.argv = ['node', 'index.js', 'img/mario.bmp', 'inverse'];
+  });
+
+  afterEach(function() {
+    process.argv = this.processBackup;
+  });
+
+  it('should have returned an array of same to parameters passing into index.js', function() {
+    var argList = cmdHandler(process.argv);
+    expect(argList[0]).to.equal('img/mario.bmp');
+    expect(argList[1]).to.equal('inverse');
   });
 });
 
+describe('test the fileHandle.js to read data', function() {
+  beforeEach(function() {
+    this.processBackup = process.argv;
+    process.argv = ['node', 'index.js', 'img/mario.bmp', 'inverse'];
+  });
 
-describe('Place Holder', function(){
-  it('Place Holder', function(){
-     expect(4+5).to.equal(9);
+  afterEach(function() {
+    process.argv = this.processBackup;
+  });
+
+  it('should have returned an array of same to parameters passing into index.js', function() {
+    var argList = cmdHandler(process.argv);
+    fileHandler.read(argList[0], function(data) {
+      var numColor = data.readUInt32LE(46);
+      if(numColor){
+        expect(numColor).to.equal(256);
+        console.log('palette');
+      }else{
+        expect(numColor).to.equal(0);
+        console.log('non-palette');
+      }
+    });
+
   });
 });
