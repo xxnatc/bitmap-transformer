a687ea9e86379e2b2caed3aab0f30afc7ab1723b
Yueyue07 <xww.qyy@gmail.com>
2016-01-11 08:44:06 -0800

    add return dataHandler process and add test for dataHandler process

--- test/test.js
+++ test/test.js
@@ -31,18 +31,68 @@ describe('test the fileHandle.js to read data', function() {
     process.argv = this.processBackup;
   });
 
-  it('should have returned an array of same to parameters passing into index.js', function() {
+  it('should have returned 256 color in palette and 0 color in non-palette', function() {
     var argList = cmdHandler(process.argv);
     fileHandler.read(argList[0], function(data) {
       var numColor = data.readUInt32LE(46);
       if(numColor){
-        expect(numColor).to.equal(256);
         console.log('palette');
+        expect(numColor).to.equal(256);
       }else{
-        expect(numColor).to.equal(0);
         console.log('non-palette');
+        expect(numColor).to.equal(0);
       }
     });
+  });
+});
 
+describe('test the fileHandle.js to read data', function() {
+  beforeEach(function() {
+    this.processBackup = process.argv;
+    process.argv = ['node', 'index.js', 'img/mario.bmp', 'inverse'];
+  });
+
+  afterEach(function() {
+    process.argv = this.processBackup;
+  });
+
+  it('it should test the bmp file is palette or non-palette', function() {
+    var argList = cmdHandler(process.argv);
+    fileHandler.read(argList[0], function(data) {
+      var numColor = data.readUInt32LE(46);
+      if(numColor){
+        console.log('palette');
+        expect(numColor).to.equal(256);
+      }else{
+        console.log('non-palette');
+        expect(numColor).to.equal(0);
+      }
+    });
+  });
+});
+
+describe('test the dataHandle.js to process data', function() {
+  beforeEach(function() {
+    this.processBackup = process.argv;
+    process.argv = ['node', 'index.js', 'img/mario.bmp', 'inverse'];
+  });
+
+  afterEach(function() {
+    process.argv = this.processBackup;
+  });
+  it('it should have returned 1024 bytes image data for palette and 30000 bytes for non-palette data', function() {
+    var argList = cmdHandler(process.argv);
+    var bitmap = {};
+    fileHandler.read(argList[0], function(data) {
+      var numColor = data.readUInt32LE(46);
+      var buf = dataHandler.process(data, bitmap);
+      if(numColor){
+        console.log('palette');
+        expect(buf).to.equal(1024);
+      }else{
+        console.log('non-palette');
+        expect(buf).to.equal(30000);
+      }
+    });
   });
 });
